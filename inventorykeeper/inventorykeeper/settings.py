6b7f1e6f9c987dd6364f1da92b60589fe52f0cc5
JingleDjango95 <spencerweber019@gmail.com>
2019-10-29 16:04:03 -0600

    heroku deployment

--- inventorykeeper/inventorykeeper/settings.py
+++ inventorykeeper/inventorykeeper/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# from .config import SECRET_API_KEY
+from .config import SECRET_API_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = SECRET_API_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', "https://testapp-sbw.herokuapp.com"]
+ALLOWED_HOSTS = ["https://sbw-django-react-app.herokuapp.com"]
 
 
 # Application definition
@@ -128,11 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'build'),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
