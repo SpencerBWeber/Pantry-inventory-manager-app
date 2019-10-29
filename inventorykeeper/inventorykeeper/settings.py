dd0beea3ea1307129b37873cde20aebf42f2f9ba
JingleDjango95 <spencerweber019@gmail.com>
2019-10-29 17:12:48 -0600

    fixed changes

--- inventorykeeper/inventorykeeper/settings.py
+++ inventorykeeper/inventorykeeper/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_API_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://sbw-django-react-app.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -128,5 +128,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
